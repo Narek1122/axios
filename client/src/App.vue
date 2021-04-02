2ff62773debd2ace5d514ae0e818561065f5cacb
narek <narek110022>
2021-04-02 22:05:25 +0400

    firs

---
+++ client/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div v-for="product in products">
+        <p>{{product.name}}</p>
+      <p>{{product.price}}</p>
+      <p>{{product.views}}</p>
+      <p>{{product.description}}</p>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      product:[];
+    }
+  },
+  mounted(){
+    this.getProducts();
+  },
+  methods:{
+    getProducts(){
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
